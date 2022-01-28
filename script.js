6600cbfb6f1b11fb1285e63afde779eaf9880598
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-01-28 09:28:49 +0400

    function draw

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 let fields = [];
 
+let currentshape = 'cross';
 
 function fillShape(id) {
-    fields[id] = 'Bild2.svg'
+    if (currentshape == 'cross') {
+        currentshape = 'star';
+    } else {
+        currentshape = 'cross';
+    }
+    fields[id] = currentshape
     console.log(fields);
+    draw();
+}
+
+function draw() {
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] == 'star') {
+            document.getElementById('star-' + i).classList.remove('d-none');
+        }
+        if (fields[i] == 'cross') {
+            document.getElementById('cross-' + i).classList.remove('d-none');
+        }
+
+    }
 }

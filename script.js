9b46c79a7c2c782c9cfad938388dab82d608797e
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-01-28 11:04:19 +0400

    checkForWin function

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function fillShape(id) {
     fields[id] = currentshape
     console.log(fields);
     draw();
+    checkForWin();
 }
 
 function draw() {
@@ -22,5 +23,40 @@ function draw() {
             document.getElementById('cross-' + i).classList.remove('d-none');
         }
 
+    }
+}
+
+function checkForWin() {
+    let winner;
+
+    if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
+        winner = fields[0];
+    }
+    if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
+        winner = fields[3];
+    }
+    if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
+        winner = fields[6];
+    }
+    if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
+        winner = fields[0];
+    }
+    if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
+        winner = fields[1];
+    }
+    if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
+        winner = fields[2];
+    }
+    if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
+        winner = fields[0];
+    }
+    if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
+        winner = fields[0];
+    }
+    if (fields[2] == fields[4] && fields[4] == fields[6]) {
+        winner = fields[2];
+    }
+    if (winner) {
+        console.log('Gewonnen', winner);
     }
 }

8e866aebd79a28da68bc9480d007c4a2508cfa69
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-01-29 17:22:41 +0400

    line-animation

--- script.js
+++ script.js
@@ -37,30 +37,35 @@ function checkForWin() {
 
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line-1').style.transform = 'scaleX(1)'
     }
     if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3];
+        document.getElementById('line-2').style.transform = 'scaleX(1)'
     }
     if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6];
+        document.getElementById('line-3').style.transform = 'scaleX(1)'
     }
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1)'
     }
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1];
+        document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1)'
     }
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2];
+        document.getElementById('line-6').style.transform = 'rotate(90deg) scaleX(1)'
     }
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line-7').style.transform = 'rotate(45deg) scaleX(1)'
     }
-    if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
-        winner = fields[0];
-    }
-    if (fields[2] == fields[4] && fields[4] == fields[6]) {
+    if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2];
+        document.getElementById('line-8').style.transform = 'rotate(-45deg) scaleX(1)'
     }
     if (winner) {
         console.log('Gewonnen', winner);
